3b99e3fd30e9c2a2eeddbe052d54d15f841eaa89
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2023-05-10 06:58:09 -0500

    added render url

--- assets/js/pages/register.js
+++ assets/js/pages/register.js
@@ -1,5 +1,6 @@
 const customerForm = document.getElementsByClassName("customer-form")[0];
-const API_URL = "http://localhost:4000/api/";
+// const API_URL = "http://localhost:4000/api/";
+const API_URL = "https://pifortune-server.onrender.com/api/";
 
 customerForm.addEventListener("submit", (e) => {
   e.preventDefault();
