7f153c8f6962ee765e0c6dc58747cfcbdcad7725
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2023-07-08 16:47:05 -0500

    added pi auth

---
+++ assets/js/pages/all.js
@@ -0,0 +1,16 @@
+const user = localStorage.getItem("user");
+
+const userAccount = document.querySelector(".user-account");
+const vendorAccount = document.querySelector(".vendor-account");
+
+userAccount.addEventListener("click", async () => {
+  const scopes = ["username", "payments"];
+  const authResult = await window.Pi.authenticate(
+    scopes,
+    onIncompletePaymentFound
+  );
+
+  console.log(authResult);
+});
+
+const getUserFromPI = () => {};
