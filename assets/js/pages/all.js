93b3d89d75475181eb8f051cd77b25f65fe2cd1f
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2023-07-10 07:52:52 -0500

    test

--- assets/js/pages/all.js
+++ assets/js/pages/all.js
@@ -3,9 +3,22 @@ const user = localStorage.getItem("user");
 const userAccount = document.querySelector(".user-account");
 const vendorAccount = document.querySelector(".vendor-account");
 
+const backendURL = _window.__ENV && _window.__ENV.backendURL;
+
+const axiosClient = axios.create({
+  baseURL: `${backendURL}`,
+  timeout: 20000,
+  withCredentials: true,
+});
+
+const onIncompletePaymentFound = (payment) => {
+  console.log("onIncompletePaymentFound", payment);
+  return axiosClient.post("/payments/incomplete", { payment });
+};
+
 userAccount.addEventListener("click", async () => {
   const scopes = ["username", "payments"];
-  const authResult = await Pi.authenticate(scopes, onIncompletePaymentFound);
+  const authResult = await Pi.authenticate(scopes);
 
   console.log(authResult);
 });
