a085425b9bae600904888333db23697e7c4ae007
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2023-07-11 11:59:11 -0500

    pi login done

--- assets/js/pages/all.js
+++ assets/js/pages/all.js
@@ -2,25 +2,28 @@ const user = localStorage.getItem("user");
 
 const userAccount = document.querySelector(".user-account");
 const vendorAccount = document.querySelector(".vendor-account");
-
-const backendURL = _window.__ENV && _window.__ENV.backendURL;
-
-const axiosClient = axios.create({
-  baseURL: `${backendURL}`,
-  timeout: 20000,
-  withCredentials: true,
-});
+const authenticatedUser = document.querySelector(".authenticated-user");
 
 const onIncompletePaymentFound = (payment) => {
   console.log("onIncompletePaymentFound", payment);
-  return axiosClient.post("/payments/incomplete", { payment });
 };
 
 userAccount.addEventListener("click", async () => {
   const scopes = ["username", "payments"];
-  const authResult = await Pi.authenticate(scopes);
+  const authResult = await window.Pi.authenticate(
+    scopes,
+    onIncompletePaymentFound
+  );
 
-  console.log(authResult);
+  const savedUser = {
+    userName: authResult.user.username,
+    accessToken: authResult.accessToken,
+  };
+
+  localStorage.setItem("user", JSON.stringify(savedUser));
+
+  authenticatedUser.textContent = `Hi ${savedUser.userName}`;
+  userAccount.style.display = "none";
 });
 
 const getUserFromPI = () => {};
