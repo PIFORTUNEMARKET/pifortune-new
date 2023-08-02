97cbd3370b5209d318cca832f1faf6e573118cfc
kennyoyaf <kehindeoyafemi@gmail.com>
2023-08-01 18:25:05 -0700

    vendoruploads fetch  done

---
+++ assets/js/pages/vendorUpload.js
@@ -0,0 +1,23 @@
+const form = document.querySelector(".vendor-upload-detail");
+
+const API_URL = isLocalhost
+  ? "http://localhost:4000/api/"
+  : "https://pifortune-server.onrender.com/api/";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
+  fetch(`${API_URL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+});
