82a6f1dcc3dcad396b20c89d70d704ea9765be80
kennyoyaf <kehindeoyafemi@gmail.com>
2023-08-14 20:24:15 -0700

    .

--- assets/js/pages/vendorUpload.js
+++ assets/js/pages/vendorUpload.js
@@ -1,4 +1,16 @@
 const form = document.querySelector(".vendor-upload-detail");
+const picture = document.querySelector("#pictures");
+
+//check if app is in development or production
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    window.location.hostname === "[::1]" ||
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+console.log(picture);
 
 const API_URL = isLocalhost
   ? "http://localhost:4000/api/"
@@ -10,33 +22,16 @@ form.addEventListener("submit", (e) => {
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
 
-  fetch(`${API_URL}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  console.log(data[picture]);
+
+  // fetch(`${API_URL}`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data))
+  //   .catch((error) => console.log(error));
 });
-
-const API_URL_PRODUCT = "http://localhost:4000/api/vendor/post/product";
-
-async function fetchDetails(data) {
-  try {
-    const response = await fetch(`${API_URL_PRODUCT}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    const result = await response.json();
-    console.log("Success:", result);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
