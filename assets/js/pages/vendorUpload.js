7397f1a06b408b3671983c76f75e60db731f5275
kennyoyaf <kehindeoyafemi@gmail.com>
2023-08-21 09:51:05 -0700

    .

--- assets/js/pages/vendorUpload.js
+++ assets/js/pages/vendorUpload.js
@@ -1,33 +1,36 @@
 const form = document.querySelector(".vendor-upload-detail");
-const picture = document.querySelector("#pictures");
+const imgContainer = document.querySelectorAll("#img-container");
+const imgPreview = document.querySelectorAll("#pictures");
+const inputIcon = document.querySelector(".pics");
 
-//check if app is in development or production
-const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
-    window.location.hostname === "[::1]" ||
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
-
-console.log(picture);
+inputIcon.addEventListener("change", () => {
+  for (let i = 0; i < inputIcon.files.length; i++) {
+    let reader = new FileReader();
+    reader.readAsDataURL(inputIcon.files[i]);
+    reader.onload = () => {
+      for (let i = 0; i < imgPreview.length; i++) {
+        imgPreview[i].setAttribute("src", reader.result);
+      }
+    };
+  }
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
+  data.pictures = inputIcon.files;
 
-  console.log(data[picture]);
-
-  // fetch(`${API_URL}`, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(data),
-  // })
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data))
-  //   .catch((error) => console.log(error));
+  fetch(`${API_URL}vendor/post/product`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${vendor}`,
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
 });
