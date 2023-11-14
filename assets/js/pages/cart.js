3f1357b114f7a0568761ca52f736a6d50861d5b5
kennyoyaf <kehindeoyafemi@gmail.com>
2023-11-14 11:52:29 -0800

    whole cart functionality done

--- assets/js/pages/cart.js
+++ assets/js/pages/cart.js
@@ -8,8 +8,8 @@ let cartRows = document.getElementsByClassName("cart__row")
 let priceElement = document.getElementsByClassName("cart-money")
 let totalPrice = document.getElementsByClassName("total-money")
 let subTotal = document.getElementsByClassName("subtotal-money")[0]
-let clearCart = document.getElementsByClassName("clear")[0]
-let emptyCartText = document.getElementsByClassName("empty-cart-text")[0]
+const clearCart = document.getElementsByClassName("clear")[0]
+const emptyCartText = document.getElementsByClassName("empty-cart-text")[0]
 
 for (let i = 0; i < cartRows.length; i++) {
   let price = parseFloat(priceElement[i].innerText.replace("$", ""))
@@ -17,40 +17,37 @@ for (let i = 0; i < cartRows.length; i++) {
   let newSubTotal = price * inputValue
 
   totalPrice[i].innerText = `$${newSubTotal.toFixed(2)}`
-  // const subTotal = newTotalPrice.map(x => x.item).reduce((x, y) => x + y, 0)
-  // console.log(subTotal)
+
+  updateCartTotal()
 
   plusElement[i].addEventListener("click", function (e) {
-    let sum = 0
     inputValue += 1
-    newValue = inputValue
-    quantityInputs[i].value = newValue
-    let calc = newValue * price
+    quantityInputs[i].value = inputValue
+    let calc = inputValue * price
     totalPrice[i].innerText = `$${calc.toFixed(2)}`
-    let combinedPrice = parseFloat(totalPrice[i].innerText.replace("$", ""))
-    // sum += combinedPrice
-    // subTotal.innerText = sum
-    // console.log((sum += combinedPrice))
+    updateCartTotal()
   })
 
   minusElement[i].addEventListener("click", function (e) {
-    let sum = 0
-    if (newValue <= 1) {
+    if (inputValue <= 1) {
       inputValue = 1
-      newValue = inputValue
     } else {
-      newValue = inputValue--
-      quantityInputs[i].value = newValue
-      let calc = newValue * price
-      // console.log(calc)
-      totalPrice[i].innerText = `$${calc.toFixed(2)}`
-      let combinedPrice = parseFloat(totalPrice[i].innerText.replace("$", ""))
-      // sum += combinedPrice
-      // subTotal.innerText = sum
-      // console.log(sum)
+      inputValue--
     }
-    console.log(totalPrice[i])
+    quantityInputs[i].value = inputValue
+    let calc = inputValue * price
+    totalPrice[i].innerText = `$${calc.toFixed(2)}`
+    updateCartTotal()
   })
+}
+
+function updateCartTotal() {
+  let sum = 0
+  for (let i = 0; i < totalPrice.length; i++) {
+    let totalPriceValue = parseFloat(totalPrice[i].innerText.replace("$", ""))
+    sum += totalPriceValue
+  }
+  subTotal.innerText = "$" + sum.toFixed(2)
 }
 
 if (itemContainer.children.length === 0) {
@@ -71,70 +68,3 @@ clearCart.addEventListener("click", e => {
   cartItemContainer.remove()
   emptyCartText.setAttribute("class", "d-block")
 })
-
-// for (let i = 0; i < quantityInputs.length; i++) {
-//   let input = quantityInputs[i]
-//   input.addEventListener("change", e => {
-//     e.preventDefault()
-//     let inputChange = e.target.value
-//     // console.log(inputChange)
-//     if (isNaN(inputChange) || inputChange <= 0) {
-//       inputChange = 1
-//     }
-//     quantity = inputChange
-//   })
-// }
-
-// for (var i = 0; i < minusElement.length; i++) {
-//   minusElement[i].addEventListener("click", function (event) {
-//     const buttonClicked = event.target
-//     console.log(buttonClicked)
-//     let input = buttonClicked.parentElement.nextElementSibling
-//     inputValue = input.value
-//     console.log(inputValue)
-//     if (isNaN(inputValue) || inputValue <= 0) {
-//       var newValue = parseInt(inputValue) - 1
-//       inputValue.innerText = newValue
-//     }
-//     // console.log(inputValue)
-//     for (let i = 0; i < cartRows.length; i++) {
-//       let cartRow = cartRows[i]
-//       let priceElement = cartRow.getElementsByClassName("money")[1]
-//       let totalPrice = cartRow.getElementsByClassName("total-money")[0]
-//       let price = parseFloat(priceElement.innerText.replace("$", ""))
-//       let quantity = inputValue
-//       let priceMultiplication = price * quantity
-//       total = priceMultiplication
-//       total = Math.round(total * 100) / 100
-//       totalPrice.innerText = `$${total}`
-//     }
-//   })
-
-//   // updateCartTotal()
-// }
-
-// for (var i = 0; i < plusElement.length; i++) {
-//   console.log(i, plusElement[i])
-//   plusElement[i].addEventListener("click", function (event) {
-//     const btnClicked = event.target
-//     console.log(i, plusElement[i])
-//     console.log(i, btnClicked)
-//     let input = btnClicked.parentElement.previousElementSibling.value
-//     let newValue = parseInt(input) + 1
-//     input.innerText = newValue
-//     console.log(input)
-
-// for (let j = 0; j < cartRows.length; j++) {
-//   let cartRow = cartRows[j]
-//   console.log(cartRow)
-//   let priceElement = cartRow.getElementsByClassName("money")[1]
-//   let totalPrice = cartRow.getElementsByClassName("total-money")[0]
-//   let price = parseFloat(priceElement.innerText.replace("$", ""))
-//   let quantity = input
-//   let priceMultiplication = price * quantity
-//   total = priceMultiplication
-//   total = Math.round(total * 100) / 100
-//   totalPrice.innerText = `$${total}`
-// }
-//   })
-// }
